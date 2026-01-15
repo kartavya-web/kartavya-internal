3ba84da8142cb1f98706cb7a534269672a8fcdf2
Amit Bhagat <iamamitbhagat384@gmail.com>
2026-01-16 03:45:23 +0530

    made change to use CORS only in development env

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,12 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(logger);
-app.use(cors(corsOptions));
+
+// We use same origin in production, so CORS is only needed in development
+if (process.env.NODE_ENV === "development") {
+  app.use(cors(corsOptions));
+}
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
